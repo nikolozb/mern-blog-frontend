57f876aa691d52152895214eb8a2fb5805a3d206
nikolozb <berose995@gmail.com>
2023-01-06 16:44:17 +0400

    routes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,6 +3,7 @@ import Button from "@mui/material/Button";
 
 import styles from "./Header.module.scss";
 import Container from "@mui/material/Container";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   const isAuth = false;
@@ -13,15 +14,15 @@ export const Header = () => {
     <div className={styles.root}>
       <Container maxWidth="lg">
         <div className={styles.inner}>
-          <a className={styles.logo} href="/">
+          <Link className={styles.logo} to="/">
             <div>MERN BLOG</div>
-          </a>
+          </Link>
           <div className={styles.buttons}>
             {isAuth ? (
               <>
-                <a href="/posts/create">
+                <Link to="/posts/create">
                   <Button variant="contained">Write post</Button>
-                </a>
+                </Link>
                 <Button
                   onClick={onClickLogout}
                   variant="contained"
@@ -32,12 +33,12 @@ export const Header = () => {
               </>
             ) : (
               <>
-                <a href="/login">
+                <Link to="/login">
                   <Button variant="outlined">Log In</Button>
-                </a>
-                <a href="/register">
-                  <Button variant="contained">Create Accout</Button>
-                </a>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Create Account</Button>
+                </Link>
               </>
             )}
           </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.scss";
 import { ThemeProvider } from "@mui/material";
@@ -13,7 +14,9 @@ root.render(
   <React.StrictMode>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
