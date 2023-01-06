f047412c7ef32f942c78b3fe491ff6e29fd3500f
nikolozb <berose995@gmail.com>
2023-01-06 19:12:37 +0400

    update: posts/fullposts logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,9 +38,10 @@ export const Home = () => {
               <Post key={index} isLoading={true} />
             ) : (
               <Post
+                key={post._id}
                 _id={post._id}
                 title={post.title}
-                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                imageUrl={post.imageUrl}
                 user={post.user}
                 createdAt={post.createdAt}
                 viewsCount={post.viewsCount}
